5dddaa110508d35993244c536c4c7fe5d2b7d290
Igor Mosyagin <i.mosyagin@lamoda.ru>
2019-01-07 00:03:01 +0300

    Fix url

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('tasks/', include('tasks.urls', namespace="tasks")),
+    path('', include('tasks.urls', namespace="tasks")),
     path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
 ]
